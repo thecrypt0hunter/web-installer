62fa19fff528fc4a90085bd63a09241176da1f6a
Hunter <thecryptohunter@protonmail.com>
2019-09-21 17:37:05 +0100

    Update vhost server block

--- install-reverse-proxy.sh
+++ install-reverse-proxy.sh
@@ -159,22 +159,20 @@ EOF
 server {
     listen        80;
     server_name ${DNS_NAME};
+
     location / {
-        proxy_pass                  http://${SERVER_IP};
-        proxy_http_version          1.1;
-        proxy_set_header Upgrade    \$http_upgrade;
-        proxy_set_header            Connection keep-alive;
-        proxy_set_header Host       \$host;
-        proxy_cache_bypass          \$http_upgrade;
-        proxy_set_header            X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header            X-Forwarded-Proto \$scheme;
-        proxy_cache_bypass                 \$http_upgrade;
-        proxy_set_header Connection        "upgrade";
-        proxy_set_header X-Real-IP         \$remote_addr;
-        proxy_set_header X-Forwarded-For   \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_set_header X-Forwarded-Host  \$host;
-        proxy_set_header X-Forwarded-Port  \$server_port;
+        proxy_pass                          http://DESTINATION_IP;
+        proxy_http_version                  1.1;
+        proxy_set_header Upgrade            \$http_upgrade;
+        proxy_set_header                    Connection keep-alive;
+        proxy_set_header Host               \$host;
+        proxy_cache_bypass                  \$http_upgrade;
+        proxy_set_header X-Real-IP          \$remote_addr;
+        proxy_set_header X-Forwarded-For    \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto  \$scheme;
+        proxy_set_header Connection         "upgrade";
+        proxy_set_header X-Forwarded-Host   \$host;
+        proxy_set_header X-Forwarded-Port   \$server_port;
     }
 }
 EOF
